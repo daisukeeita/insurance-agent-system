ca1b59ea0130a7ddb094b3d0901d07831f6d3b93
JAVV <vinas.johnandrei@gmail.com>
2025-11-06 21:27:01 +0800

    interface for agent repository

---
+++ src/main/java/com/acolyptos/insurance/domain/agent/AgentRepositoryInterface.java
@@ -0,0 +1,19 @@
+// package com.acolyptos.insurance.domain.agent;
+//
+// import java.util.List;
+// import java.util.Optional;
+//
+// public interface AgentRepositoryInterface {
+//
+//   Agent registerAgent(Agent agent);
+//
+//   Optional<Agent> findAgentByUsername(String username);
+//
+//   Optional<Agent> findAgentByLicenseNumber(String licenseNumber);
+//
+//   List<Agent> findAllAgents();
+//
+//   void disableAgentByUsername(String username);
+//
+//   void disableAgentByLicenseNumber(String licenseNumber);
+// }
