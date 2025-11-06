76e59daa79066361e7c930bf13e5763b1358d03f
JAVV <vinas.johnandrei@gmail.com>
2025-11-06 21:29:12 +0800

    repository for agent that extends/uses JPA interface

---
+++ src/main/java/com/acolyptos/insurance/infrastructure/persistence/agent/AgentJpaRepository.java
@@ -0,0 +1,15 @@
+// package com.acolyptos.insurance.infrastructure.persistence.agent;
+//
+// import com.acolyptos.insurance.domain.agent.Agent;
+// import java.util.Optional;
+// import java.util.UUID;
+// import org.springframework.data.jpa.repository.JpaRepository;
+//
+// public interface AgentJpaRepository extends JpaRepository<Agent, UUID> {
+//
+//   Agent save(Agent agent);
+//
+//   Optional<Agent> findByUsername(String username);
+//
+//   Optional<Agent> findByLicenseNumber(String licenseNumber);
+// }
