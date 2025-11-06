3dab7ccfbd5b720dd6b6a980a88ec69a330c9def
JAVV <vinas.johnandrei@gmail.com>
2025-11-06 21:31:29 +0800

    entry point of the application

---
+++ src/main/java/com/acolyptos/insurance/InsuranceAgentSystem.java
@@ -0,0 +1,12 @@
+package com.acolyptos.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceAgentSystem {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InsuranceAgentSystem.class, args);
+  }
+}
