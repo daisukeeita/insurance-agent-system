b5b25c9be4b51c2e8af1276e94fd3cf4ef807e08
JAVV <vinas.johnandrei@gmail.com>
2025-11-06 21:30:11 +0800

    repository for insurer that extends/uses JPA interface

---
+++ src/main/java/com/acolyptos/insurance/infrastructure/persistence/insurer/InsurerJpaRepository.java
@@ -0,0 +1,13 @@
+package com.acolyptos.insurance.infrastructure.persistence.insurer;
+
+import com.acolyptos.insurance.domain.insurer.Insurer;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InsurerJpaRepository extends JpaRepository<Insurer, UUID> {
+
+  Insurer save(Insurer insurer);
+
+  Optional<Insurer> findByInsurerName(String insurerName);
+}
