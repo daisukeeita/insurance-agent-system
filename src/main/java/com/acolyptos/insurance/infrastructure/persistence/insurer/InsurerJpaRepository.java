2d3117c0512a9e0004b54322ed55d1101137e068
JAVV <vinas.johnandrei@gmail.com>
2025-11-08 21:26:36 +0800

    modified return type of a method

--- src/main/java/com/acolyptos/insurance/infrastructure/persistence/insurer/InsurerJpaRepository.java
+++ src/main/java/com/acolyptos/insurance/infrastructure/persistence/insurer/InsurerJpaRepository.java
@@ -1,7 +1,6 @@
 package com.acolyptos.insurance.infrastructure.persistence.insurer;
 
 import com.acolyptos.insurance.domain.insurer.Insurer;
-import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +8,5 @@ public interface InsurerJpaRepository extends JpaRepository<Insurer, UUID> {
 
   Insurer save(Insurer insurer);
 
-  Optional<Insurer> findByInsurerName(String insurerName);
+  Insurer findByInsurerName(String insurerName);
 }
