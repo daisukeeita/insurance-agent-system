781c7242aaf4bba9888e92be4f8c89ddd42bbfe7
JAVV <vinas.johnandrei@gmail.com>
2025-11-08 21:25:59 +0800

    Response class for successful process

---
+++ src/main/java/com/acolyptos/insurance/domain/response/ErrorResponse.java
@@ -0,0 +1,52 @@
+package com.acolyptos.insurance.domain.response;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+
+public class ErrorResponse {
+
+  private final boolean success = false;
+  private final int code;
+  private final HttpStatus httpStatus;
+  private final Map<String, String> errors;
+  private final LocalDateTime timestamp = LocalDateTime.now();
+
+  public ErrorResponse(
+      final int code, final HttpStatus httpStatus, final Map<String, String> errors) {
+    this.code = code;
+    this.httpStatus = httpStatus;
+    this.errors = errors;
+  }
+
+  public int getCode() {
+    return code;
+  }
+
+  public HttpStatus getHttpStatus() {
+    return httpStatus;
+  }
+
+  public Map<String, String> getErrors() {
+    return errors;
+  }
+
+  public LocalDateTime getLocalDateTime() {
+    return timestamp;
+  }
+
+  @Override
+  public String toString() {
+    return "ErrorResponse {\n\tsuccess: "
+        + success
+        + ",\n\tcode: "
+        + code
+        + ",\n\thttpStatus: "
+        + httpStatus
+        + ",\n\terrors: "
+        + errors
+        + ",\n\ttimestamp: "
+        + timestamp
+        + "\n}";
+  }
+}
