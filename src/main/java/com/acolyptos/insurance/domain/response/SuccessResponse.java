bd3a1083b1c6b19214fcab558ca8b95332e06ebb
JAVV <vinas.johnandrei@gmail.com>
2025-11-08 21:26:18 +0800

    Response class for invalid/error process/es

---
+++ src/main/java/com/acolyptos/insurance/domain/response/SuccessResponse.java
@@ -0,0 +1,55 @@
+package com.acolyptos.insurance.domain.response;
+
+import java.time.LocalDateTime;
+import org.springframework.http.HttpStatus;
+
+public class SuccessResponse<T> {
+
+  private final boolean success = true;
+  private final int code;
+  private final HttpStatus httpStatus;
+  private final String message;
+  private final T data;
+  private final LocalDateTime timestamp = LocalDateTime.now();
+
+  public SuccessResponse(
+      final int code, final HttpStatus httpStatus, final String message, final T data) {
+    this.code = code;
+    this.httpStatus = httpStatus;
+    this.message = message;
+    this.data = data;
+  }
+
+  public int getCode() {
+    return code;
+  }
+
+  public HttpStatus getHttpStatus() {
+    return httpStatus;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public T getData() {
+    return data;
+  }
+
+  @Override
+  public String toString() {
+    return "SuccessResponse {\n\tsuccess: "
+        + success
+        + ",\n\tcode: "
+        + code
+        + ",\n\thttpStatus: "
+        + httpStatus
+        + ",\n\tmessage: "
+        + message
+        + ",\n\tdata: "
+        + data
+        + ",\n\ttimestamp: "
+        + timestamp
+        + "\n}";
+  }
+}
