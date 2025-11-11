6aa64665a2d611b4cda8015b340c7694f06f9c9d
JAVV <vinas.johnandrei@gmail.com>
2025-11-11 14:04:07 +0800

    added comments

--- src/main/java/com/acolyptos/insurance/domain/insurer/InsurerRegisterRequest.java
+++ src/main/java/com/acolyptos/insurance/domain/insurer/InsurerRegisterRequest.java
@@ -2,6 +2,7 @@ package com.acolyptos.insurance.domain.insurer;
 
 import jakarta.validation.constraints.NotBlank;
 
+/** Entity that represents the request body for insurer registration. */
 public class InsurerRegisterRequest {
 
   @NotBlank(message = "Insurer Name is required for registration.")
@@ -10,8 +11,16 @@ public class InsurerRegisterRequest {
   @NotBlank(message = "Insurer Address is required for registration.")
   private String insurerAddress;
 
+  /** Protecting the default constructor of the class. */
   protected InsurerRegisterRequest() {}
 
+  /**
+   * Constructor that will be used when initializing the class.
+   *
+   * @param insurerName represents passed name from the client that will be used to register/save.
+   * @param insurerAddress represents passed address from the client that will be used to
+   *     register/save.
+   */
   public InsurerRegisterRequest(final String insurerName, final String insurerAddress) {
     this.insurerName = insurerName;
     this.insurerAddress = insurerAddress;
