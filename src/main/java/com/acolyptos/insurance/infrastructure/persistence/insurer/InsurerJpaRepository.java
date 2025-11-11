5bff09937956d7e1530debe72d97d0a4d228a406
JAVV <vinas.johnandrei@gmail.com>
2025-11-11 14:04:21 +0800

    added comments

--- src/main/java/com/acolyptos/insurance/infrastructure/persistence/insurer/InsurerJpaRepository.java
+++ src/main/java/com/acolyptos/insurance/infrastructure/persistence/insurer/InsurerJpaRepository.java
@@ -4,9 +4,25 @@ import com.acolyptos.insurance.domain.insurer.Insurer;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Interface class that extends the {@link JpaRepository} to be able to use simplified data access
+ * methods.
+ */
 public interface InsurerJpaRepository extends JpaRepository<Insurer, UUID> {
 
+  /**
+   * Custom Jpa repository method that saves an entity.
+   *
+   * @param insurer passed object that will be used to save an entity.
+   * @return {@link Insurer} if the process is successful.
+   */
   Insurer save(Insurer insurer);
 
+  /**
+   * Custom Jpa repository method that finds an entity using the passed string.
+   *
+   * @param insurerName passed string that will be used to find the entity from the database.
+   * @return {@link Insurer} if the process is successful, else null.
+   */
   Insurer findByInsurerName(String insurerName);
 }
