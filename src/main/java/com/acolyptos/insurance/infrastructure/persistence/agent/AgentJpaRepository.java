866aa18ace550cbe94cbb53ea7d22072562a0d98
JAVV <vinas.johnandrei@gmail.com>
2025-11-12 22:02:36 +0800

    uncommented the class

--- src/main/java/com/acolyptos/insurance/infrastructure/persistence/agent/AgentJpaRepository.java
+++ src/main/java/com/acolyptos/insurance/infrastructure/persistence/agent/AgentJpaRepository.java
@@ -1,15 +1,14 @@
-// package com.acolyptos.insurance.infrastructure.persistence.agent;
-//
-// import com.acolyptos.insurance.domain.agent.Agent;
-// import java.util.Optional;
-// import java.util.UUID;
-// import org.springframework.data.jpa.repository.JpaRepository;
-//
-// public interface AgentJpaRepository extends JpaRepository<Agent, UUID> {
-//
-//   Agent save(Agent agent);
-//
-//   Optional<Agent> findByUsername(String username);
-//
-//   Optional<Agent> findByLicenseNumber(String licenseNumber);
-// }
+package com.acolyptos.insurance.infrastructure.persistence.agent;
+
+import com.acolyptos.insurance.domain.agent.Agent;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgentJpaRepository extends JpaRepository<Agent, UUID> {
+
+  Agent save(Agent agent);
+
+  Agent findByUsername(String username);
+
+  Agent findByLicenseNumber(String licenseNumber);
+}
