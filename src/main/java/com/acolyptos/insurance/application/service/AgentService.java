a97fa89a8c9ae54254b8f02bd992e91b39084101
JAVV <vinas.johnandrei@gmail.com>
2025-11-14 11:16:46 +0800

    added comments for explanation

--- src/main/java/com/acolyptos/insurance/application/service/AgentService.java
+++ src/main/java/com/acolyptos/insurance/application/service/AgentService.java
@@ -4,6 +4,7 @@ import com.acolyptos.insurance.domain.agent.Agent;
 import com.acolyptos.insurance.domain.agent.AgentLoginRequest;
 import com.acolyptos.insurance.domain.agent.AgentRegisterRequest;
 import com.acolyptos.insurance.domain.agent.AgentRepositoryInterface;
+import com.acolyptos.insurance.domain.agent.AgentResponse;
 import com.acolyptos.insurance.domain.exceptions.EntityAlreadyExistsException;
 import com.acolyptos.insurance.domain.exceptions.EntityDoesNotExistException;
 import com.acolyptos.insurance.domain.exceptions.InvalidRequestBodyException;
@@ -12,19 +13,38 @@ import java.util.List;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
 
+/** Service class for agent entity. */
 @Service
 public class AgentService {
 
   private final AgentRepositoryInterface agentRepositoryInterface;
   private final InsurerService insurerService;
 
+  /**
+   * Constructor for Agent Service class.
+   *
+   * @param agentRepositoryInterface injected interface to have an access to database.
+   * @param insurerService injected service class for insurer to have an access to insurer entity.
+   */
   public AgentService(
       AgentRepositoryInterface agentRepositoryInterface, InsurerService insurerService) {
     this.agentRepositoryInterface = agentRepositoryInterface;
     this.insurerService = insurerService;
   }
 
-  public Agent createAgent(AgentRegisterRequest agentRegisterRequest) {
+  /**
+   * Method to process and validate the {@link AgentRegisterRequest} and saves it to the database.
+   * It'll return a filtered detail of the {@link Agent} upon successful process.
+   *
+   * @param agentRegisterRequest the request body of the client that will be processed and
+   *     validated.
+   * @return a filtered {@link AgentResponse} if the process is successful.
+   * @throws EntityAlreadyExistsException if the agent already exists after checking from the
+   *     database.
+   * @throws EntityDoesNotExistException if the insurer does not exists after checking to the
+   *     database.
+   */
+  public AgentResponse createAgent(AgentRegisterRequest agentRegisterRequest) {
     Agent checkIfExists =
         agentRepositoryInterface.findAgentByUsername(agentRegisterRequest.getUsername());
     if (checkIfExists != null) {
@@ -48,9 +68,19 @@ public class AgentService {
     agent.setLastName(agentRegisterRequest.getLastName());
     agent.setLicenseNumber(agentRegisterRequest.getLicenseNumber());
 
-    return agentRepositoryInterface.registerAgent(agent);
+    Agent savedAgent = agentRepositoryInterface.registerAgent(agent);
+
+    AgentResponse agentResponse = new AgentResponse();
+    agentResponse.setAgentId(savedAgent.getAgentId());
+    agentResponse.setUsername(savedAgent.getUsername());
+    agentResponse.setInsurer(savedAgent.getInsurer());
+    agentResponse.setFullName(savedAgent.getFullName());
+    agentResponse.setLicenseNumber(savedAgent.getLicenseNumber());
+
+    return agentResponse;
   }
 
+  /** Method for authenticating the agent. */
   public Agent loginAgent(AgentLoginRequest agentLoginRequest) {
     Agent agent = agentRepositoryInterface.findAgentByUsername(agentLoginRequest.getUsername());
 
@@ -62,7 +92,15 @@ public class AgentService {
     return agentRepositoryInterface.findAgentByUsername(agentLoginRequest.getUsername());
   }
 
-  public Agent getAgentByUsername(String username) {
+  /**
+   * Method to look for an Agent using its username.
+   *
+   * @param username the string that will be used to look for the agent.
+   * @return the filtered {@link AgentResponse} if the query is successful.
+   * @throws InvalidRequestBodyException if the username is empty or null.
+   * @throws EntityDoesNotExistException if there's no agent found from the database.
+   */
+  public AgentResponse getAgentByUsername(String username) {
 
     if (username.trim().isEmpty() || username == null) {
       throw new InvalidRequestBodyException("Agent's username is required.");
@@ -75,10 +113,25 @@ public class AgentService {
           "Cannot find an Agent with username: " + "'" + username + "'.");
     }
 
-    return agent;
+    AgentResponse agentResponse = new AgentResponse();
+    agentResponse.setAgentId(agent.getAgentId());
+    agentResponse.setUsername(agent.getUsername());
+    agentResponse.setFullName(agent.getFullName());
+    agentResponse.setLicenseNumber(agent.getLicenseNumber());
+    agentResponse.setInsurer(agent.getInsurer());
+
+    return agentResponse;
   }
 
-  public Agent getAgentByLicenseNumber(String licenseNumber) {
+  /**
+   * Method to look for an Agent using its username.
+   *
+   * @param licenseNumber the string that will be used to look for the agent.
+   * @return the filtered {@link AgentResponse} if the query is successful.
+   * @throws InvalidRequestBodyException if the username is empty or null.
+   * @throws EntityDoesNotExistException if there's no agent found from the database.
+   */
+  public AgentResponse getAgentByLicenseNumber(String licenseNumber) {
 
     if (licenseNumber.trim().isEmpty() || licenseNumber == null) {
       throw new InvalidRequestBodyException("Agent's license number is required.");
@@ -91,7 +144,14 @@ public class AgentService {
           "Cannot find an Agent with license number: " + "'" + licenseNumber + "'.");
     }
 
-    return agent;
+    AgentResponse agentResponse = new AgentResponse();
+    agentResponse.setAgentId(agent.getAgentId());
+    agentResponse.setUsername(agent.getUsername());
+    agentResponse.setFullName(agent.getFullName());
+    agentResponse.setLicenseNumber(agent.getLicenseNumber());
+    agentResponse.setInsurer(agent.getInsurer());
+
+    return agentResponse;
   }
 
   public List<Agent> getAllAgents() {
