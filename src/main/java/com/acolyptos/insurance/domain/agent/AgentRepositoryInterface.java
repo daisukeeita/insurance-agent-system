ada8307ba9c214351aa52d087c9c64c8301fff28
JAVV <vinas.johnandrei@gmail.com>
2025-11-14 11:17:49 +0800

    added comments for explanation

--- src/main/java/com/acolyptos/insurance/domain/agent/AgentRepositoryInterface.java
+++ src/main/java/com/acolyptos/insurance/domain/agent/AgentRepositoryInterface.java
@@ -2,14 +2,38 @@ package com.acolyptos.insurance.domain.agent;
 
 import java.util.List;
 
+/** The blueprint that will be used for Agent Repository. */
 public interface AgentRepositoryInterface {
 
+  /**
+   * Method for saving the details of the agent to the database.
+   *
+   * @param agent entity that will be saved to the databse.
+   * @return {@link Agent} details of the agent upon successful database process.
+   */
   Agent registerAgent(Agent agent);
 
+  /**
+   * Method for looking to a specific agent from the database.
+   *
+   * @param username string that will be used to look for that agent.
+   * @return {@link Agent} details of the agent upon successful database process.
+   */
   Agent findAgentByUsername(String username);
 
+  /**
+   * Method for looking to a specific agent from the database.
+   *
+   * @param licenseNumber string that will be used to look for that agent.
+   * @return {@link Agent} details of the agent upon successful database process.
+   */
   Agent findAgentByLicenseNumber(String licenseNumber);
 
+  /**
+   * Method for returning the lists of all registered agents.
+   *
+   * @return {@link Agent} lists details of the agent upon successful process.
+   */
   List<Agent> findAllAgents();
 
   // void disableAgentByUsername(String username);
