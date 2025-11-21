f4fcb6021e30579bc6eb247ae41c20372a3434a9
JAVV <vinas.johnandrei@gmail.com>
2025-11-21 16:09:10 +0800

    added batch saving and pagination method

--- src/main/java/com/acolyptos/insurance/domain/inventory/CocRepositoryInterface.java
+++ src/main/java/com/acolyptos/insurance/domain/inventory/CocRepositoryInterface.java
@@ -1,14 +1,21 @@
 package com.acolyptos.insurance.domain.inventory;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CocRepositoryInterface {
 
   CertificateOfCoverage saveCertificateOfCoverage(CertificateOfCoverage certificateOfCoverage);
 
-  // CertificateOfCoverage getCertificateOfCoverageByNumber(String cocNumber);
-  //
-  // CertificateOfCoverage updateStatusOfCertificate();
-  //
+  List<CertificateOfCoverage> saveAllCertificateOfCoverage(
+      List<CertificateOfCoverage> listCertificateOfCoverage);
+
+  CertificateOfCoverage getCertificateOfCoverageByNumber(String cocNumber);
+
   List<CertificateOfCoverage> getAllCertificateOfCoverage();
+
+  Page<CertificateOfCoverage> getPaginatedCertificateOfCoverage(Pageable pageable);
+
+  // CertificateOfCoverage updateStatusOfCertificate();
 }
