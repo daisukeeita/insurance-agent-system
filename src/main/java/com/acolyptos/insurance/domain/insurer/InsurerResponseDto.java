e069c45c8ac40eef54b44b9f02c0d1ef6450a608
JAVV <vinas.johnandrei@gmail.com>
2025-11-27 21:09:17 +0800

    response DTO for Insurer entity

---
+++ src/main/java/com/acolyptos/insurance/domain/insurer/InsurerResponseDto.java
@@ -0,0 +1,46 @@
+package com.acolyptos.insurance.domain.insurer;
+
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ * Response DTO class for Insurer entity. This DTO is typically used to carry the necessary
+ * information that will be sent to the client.
+ */
+public class InsurerResponseDto {
+
+  @NotBlank(message = "Inusurer ID must be included to the Insurer Response DTO.")
+  private String insurerId;
+
+  @NotBlank(message = "Insurer Name must be included to the Insurer Response DTO.")
+  private String insurerName;
+
+  @NotBlank(message = "Insurer Address must be included to the Insurer Response DTO.")
+  private String insurerAddress;
+
+  /** Constructor for creating a new InsurerResponseDto. */
+  public InsurerResponseDto() {}
+
+  public void setInsurerId(final String insurerId) {
+    this.insurerId = insurerId;
+  }
+
+  public void setInsurerName(final String insurerName) {
+    this.insurerName = insurerName;
+  }
+
+  public void setInsurerAddress(final String insurerAddress) {
+    this.insurerAddress = insurerAddress;
+  }
+
+  public String getInsurerId() {
+    return insurerId;
+  }
+
+  public String getInsurerName() {
+    return insurerName;
+  }
+
+  public String getInsurerAddress() {
+    return insurerAddress;
+  }
+}
