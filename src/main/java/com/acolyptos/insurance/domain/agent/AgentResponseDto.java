9574d0ac0b859f2efaba2d813897c3c98bfcdf28
JAVV <vinas.johnandrei@gmail.com>
2025-11-29 11:53:49 +0800

    changed file name from AgentResponse to AgentResponseDto.

---
+++ src/main/java/com/acolyptos/insurance/domain/agent/AgentResponseDto.java
@@ -0,0 +1,62 @@
+package com.acolyptos.insurance.domain.agent;
+
+
+/**
+ * Class that will be used to filter the details of the Agent entity before sending the Success
+ * Response.
+ */
+public class AgentResponse {
+
+  private String agentId;
+
+  private String username;
+
+  private String fullName;
+
+  private String licenseNumber;
+
+  private String insurerName;
+
+  /** The default constructor for this class. */
+  public AgentResponse() {}
+
+  public void setAgentId(String agentId) {
+    this.agentId = agentId;
+  }
+
+  public void setInsurerName(String insurerName) {
+    this.insurerName = insurerName;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  public void setLicenseNumber(String licenseNumber) {
+    this.licenseNumber = licenseNumber;
+  }
+
+  public String getAgentId() {
+    return agentId;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public String getLicenseNumber() {
+    return licenseNumber;
+  }
+
+  public String getInsurerName() {
+    return insurerName;
+  }
+}
