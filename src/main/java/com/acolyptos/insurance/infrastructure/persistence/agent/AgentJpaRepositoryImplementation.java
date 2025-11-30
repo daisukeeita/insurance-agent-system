474b4996e2d1926452ac7487f393ed288fab2408
JAVV <vinas.johnandrei@gmail.com>
2025-11-30 21:32:05 +0800

    added comments for documentation.

--- src/main/java/com/acolyptos/insurance/infrastructure/persistence/agent/AgentJpaRepositoryImplementation.java
+++ src/main/java/com/acolyptos/insurance/infrastructure/persistence/agent/AgentJpaRepositoryImplementation.java
@@ -2,6 +2,7 @@ package com.acolyptos.insurance.infrastructure.persistence.agent;
 
 import com.acolyptos.insurance.domain.agent.Agent;
 import com.acolyptos.insurance.domain.agent.AgentRepositoryInterface;
+import com.acolyptos.insurance.domain.insurer.InsurerRepositoryInterface;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -9,11 +10,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+/**
+ * The implementation of the {@link InsurerRepositoryInterface} that uses {@link
+ * InsurerJpaRepository} to interact with the database.
+ */
 @Repository
 public class AgentJpaRepositoryImplementation implements AgentRepositoryInterface {
 
   private final AgentJpaRepository agentJpaRepository;
 
+  /**
+   * Constructs the repository implementation and injects the necessary JPA component.
+   *
+   * @param agentJpaRepository The Spring Data JPA repository used for direct database access.
+   */
   public AgentJpaRepositoryImplementation(AgentJpaRepository agentJpaRepository) {
     this.agentJpaRepository = agentJpaRepository;
   }
