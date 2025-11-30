911633803cfc28e04db974ec9f87876a75de98b0
JAVV <vinas.johnandrei@gmail.com>
2025-11-30 21:19:18 +0800

    modified comments, changed return types to some methoods

--- src/main/java/com/acolyptos/insurance/domain/agent/AgentRepositoryInterface.java
+++ src/main/java/com/acolyptos/insurance/domain/agent/AgentRepositoryInterface.java
@@ -1,51 +1,82 @@
 package com.acolyptos.insurance.domain.agent;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-/** The blueprint that will be used for Agent Repository. */
+/** Repository interface for managing {@link Agent} entities. */
 public interface AgentRepositoryInterface {
 
   /**
-   * Method for saving the details of the agent to the database.
+   * Persists the given {@link Agent} entity to the database.
    *
-   * @param agent entity that will be saved to the databse.
-   * @return {@link Agent} details of the agent upon successful database process.
+   * @param agent The entity to be saved.
+   * @return The saved {@link Agent} entity.
    */
-  Agent registerAgent(Agent agent);
+  Agent saveAgent(Agent agent);
 
   /**
-   * Method for looking to a specific agent from the database.
+   * Finds an single {@link Agent} entity by its name.
    *
-   * @param username string that will be used to look for that agent.
-   * @return {@link Agent} details of the agent upon successful database process.
+   * @param username The unique username credential of the agent to find.
+   * @return An {@link Optional} containing the {@link Agent} if found, otherwise empty.
    */
-  Agent findAgentByUsername(String username);
+  Optional<Agent> getAgentByUsername(String username);
 
   /**
-   * Method for looking to a specific agent from the database.
+   * Finds a single {@link Agent} entity by its license number.
    *
-   * @param licenseNumber string that will be used to look for that agent.
-   * @return {@link Agent} details of the agent upon successful database process.
+   * @param licenseNumber The licence number of the agent to find.
+   * @return An {@link Optional} containing the {@link Agent} if found, otherwise empty.
    */
-  Agent findAgentByLicenseNumber(String licenseNumber);
+  Optional<Agent> findAgentByLicenseNumber(String licenseNumber);
 
   /**
-   * Method for looking to a specific agent from the database.
+   * Finds a single {@link Agent} entity by its unique ID.
    *
-   * @param agentId string that will be used to look for a specific agent.
-   * @return {@link Agent} details of the agent upon successful database process.
+   * @param agentId The unique ID of the agent to find.
+   * @return An {@link Optional} containing the {@link Agent if found, otherwise empty.}
    */
-  Agent findAgentById(UUID agentId);
+  Optional<Agent> findAgentById(UUID agentId);
 
   /**
-   * Method for returning the lists of all registered agents.
+   * Checks if an {@link Agent} entity with the given unique ID exists in the database.
    *
-   * @return {@link Agent} lists details of the agent upon successful process.
+   * @param agentId The unique ID of the agent to check for existence.
+   * @return 'true' if an agent exists with te given unique ID, otherwise 'false'.
+   */
+  boolean checkAgentIfExistsById(UUID agentId);
+
+  /**
+   * Checks if an {@link Agent} entity with the given username exists in the database.
+   *
+   * @param username The username of the agent to check for existence.
+   * @return 'true' if an agent exists with the given username, otherwise 'false'.
+   */
+  boolean checkAgentIfExistsByUsername(String username);
+
+  /**
+   * Checks if an {@link Agent} entity with the given license number exists in the database.
+   *
+   * @param licenseNumber The license number of the agent to check for existence.
+   * @return 'true' if an agent exists with the given license number, otherwise 'false'.
+   */
+  boolean checkAgentIfExistsByLicenseNumber(String licenseNumber);
+
+  /**
+   * Retrieves a paginated list of all {@link Agent} entities.
+   *
+   * @param pageable The pagination information of (number, size, sort).
+   * @return A {@link Page} of {@link Agent} entities.
+   */
+  Page<Agent> getPaginatedAgents(Pageable pageable);
+
+  /**
+   * Retrieves all {@link Agent} entities from the database.
+   *
+   * @return A list of all {@link Agent} entities.
    */
   List<Agent> findAllAgents();
-
-  // void disableAgentByUsername(String username);
-  //
-  // void disableAgentByLicenseNumber(String licenseNumber);
 }
