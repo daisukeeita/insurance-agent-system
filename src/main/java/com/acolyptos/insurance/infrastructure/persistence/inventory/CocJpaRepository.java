f09692649f7c5b3750686fcb639c46cedb7decef
JAVV <vinas.johnandrei@gmail.com>
2025-12-02 13:45:21 +0800

    changed name from CocJpaRepository to CertificateJpaRepository.

--- src/main/java/com/acolyptos/insurance/infrastructure/persistence/inventory/CocJpaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.acolyptos.insurance.infrastructure.persistence.inventory;
-
-import com.acolyptos.insurance.domain.inventory.CertificateOfCoverage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CocJpaRepository extends JpaRepository<CertificateOfCoverage, String> {}
