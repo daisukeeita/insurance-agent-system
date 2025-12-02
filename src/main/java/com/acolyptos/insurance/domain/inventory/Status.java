68e7357becaad1d15f62b65b77e905cbdd2639cc
JAVV <vinas.johnandrei@gmail.com>
2025-12-02 13:23:06 +0800

    changed name from Status to CertificateStatus.

--- src/main/java/com/acolyptos/insurance/domain/inventory/Status.java
+++
@@ -1,9 +0,0 @@
-package com.acolyptos.insurance.domain.inventory;
-
-/** This will be used for the status of {@link CertificateOfCoverage} entity. */
-public enum Status {
-  AVAILABLE,
-  PENDING,
-  ACTIVE,
-  VOID
-}
