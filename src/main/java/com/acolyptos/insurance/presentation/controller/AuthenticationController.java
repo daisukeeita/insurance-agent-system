755cf0e7c1ebf4e990c52fb44fd017df3a6cca36
JAVV <vinas.johnandrei@gmail.com>
2025-12-02 14:19:12 +0800

    init

--- src/main/java/com/acolyptos/insurance/presentation/controller/AuthenticationController.java
+++ src/main/java/com/acolyptos/insurance/presentation/controller/AuthenticationController.java
@@ -2,7 +2,7 @@ package com.acolyptos.insurance.presentation.controller;
 
 import com.acolyptos.insurance.application.service.AgentService;
 import com.acolyptos.insurance.domain.agent.Agent;
-import com.acolyptos.insurance.domain.agent.AgentLoginRequest;
+import com.acolyptos.insurance.domain.agent.AgentLoginRequestDto;
 import com.acolyptos.insurance.domain.response.SuccessResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -25,7 +25,7 @@ public class AuthenticationController {
   @PostMapping("/loginUser")
   @ResponseStatus(HttpStatus.ACCEPTED)
   public SuccessResponse<Agent> authenticateAgent(
-      @RequestBody @Valid AgentLoginRequest agentLoginRequest) {
+      @RequestBody @Valid AgentLoginRequestDto agentLoginRequest) {
     Agent agent = agentService.loginAgent(agentLoginRequest);
 
     return new SuccessResponse<Agent>(
