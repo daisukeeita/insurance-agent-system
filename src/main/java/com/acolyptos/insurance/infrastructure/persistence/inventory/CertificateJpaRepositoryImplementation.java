642673bc30f78300cb422aceaabb8ca2806ef6d5
JAVV <vinas.johnandrei@gmail.com>
2025-12-02 13:45:01 +0800

    changed file name from CocJpaRepositoryImplementation to CertificateJpaRepositoryImplementation, modified methods, modified comments for documentation.

---
+++ src/main/java/com/acolyptos/insurance/infrastructure/persistence/inventory/CertificateJpaRepositoryImplementation.java
@@ -0,0 +1,60 @@
+package com.acolyptos.insurance.infrastructure.persistence.inventory;
+
+import com.acolyptos.insurance.domain.inventory.CertificateOfCoverage;
+import com.acolyptos.insurance.domain.inventory.CertificateRepositoryInterface;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+/**
+ * The implementation of the {@link CertificateRepositoryInterface} that uses {@link
+ * CertificateJpaRepository} to interact with the database.
+ */
+@Repository
+public class CertificateJpaRepositoryImplementation implements CertificateRepositoryInterface {
+
+  private final CertificateJpaRepository certificateJpaRepository;
+
+  /**
+   * Constructs the repository implementation and injects to necessary JPA component.
+   *
+   * @param certificateJpaRepository The Spring Data JPA repository used for direct database access.
+   */
+  public CertificateJpaRepositoryImplementation(CertificateJpaRepository certificateJpaRepository) {
+    this.certificateJpaRepository = certificateJpaRepository;
+  }
+
+  @Override
+  public CertificateOfCoverage saveCertificateOfCoverage(
+      CertificateOfCoverage certificateOfCoverage) {
+    return certificateJpaRepository.save(certificateOfCoverage);
+  }
+
+  @Override
+  public List<CertificateOfCoverage> saveAllCertificateOfCoverage(
+      List<CertificateOfCoverage> listCertificateOfCoverage) {
+    return certificateJpaRepository.saveAll(listCertificateOfCoverage);
+  }
+
+  @Override
+  public Optional<CertificateOfCoverage> getCertificateOfCoverageById(String cocNumber) {
+    return certificateJpaRepository.findById(cocNumber);
+  }
+
+  @Override
+  public boolean checkCertificateIfExistsById(String cocNumber) {
+    return certificateJpaRepository.existsById(cocNumber);
+  }
+
+  @Override
+  public Page<CertificateOfCoverage> getPaginatedCertificateOfCoverage(Pageable pageable) {
+    return certificateJpaRepository.findAll(pageable);
+  }
+
+  @Override
+  public List<CertificateOfCoverage> getAllCertificateOfCoverage() {
+    return certificateJpaRepository.findAll();
+  }
+}
