03833c5c55c7ce7a13f2aed6057016dfacba2715
JAVV <vinas.johnandrei@gmail.com>
2025-12-03 10:31:13 +0800

    added comments for documentation.

--- src/main/java/com/acolyptos/insurance/presentation/controller/CertificateController.java
+++ src/main/java/com/acolyptos/insurance/presentation/controller/CertificateController.java
@@ -1,94 +1,49 @@
 package com.acolyptos.insurance.presentation.controller;
 
-import com.acolyptos.insurance.application.service.InventoryService;
-import com.acolyptos.insurance.domain.inventory.CocPageResponse;
-import com.acolyptos.insurance.domain.inventory.CocRequestBody;
-import com.acolyptos.insurance.domain.inventory.CocResponseBody;
+import com.acolyptos.insurance.application.service.CertificateService;
+import com.acolyptos.insurance.domain.certificate.CertificateRequestDto;
+import com.acolyptos.insurance.domain.certificate.CertificateResponseDto;
 import com.acolyptos.insurance.domain.response.SuccessResponse;
 import jakarta.validation.Valid;
-import java.util.List;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller class for handling incoming HTTP requests related to {@link CertificateOfCoverage}
+ * operations.
+ */
 @RestController
 @RequestMapping("/api/v1/inventory")
-public class InventoryController {
+public class CertificateController {
 
-  private final InventoryService inventoryService;
+  private final CertificateService certificateService;
 
-  public InventoryController(InventoryService inventoryService) {
-    this.inventoryService = inventoryService;
+  /**
+   * Constructs a {@code CertificateController} with required service dependency.
+   *
+   * @param certificateService The service class implementing the business logic for Certificate of
+   *     Coverage operations.
+   */
+  public CertificateController(CertificateService certificateService) {
+    this.certificateService = certificateService;
   }
 
   @PostMapping("/addCertificateOfCoverage")
   @ResponseStatus(HttpStatus.CREATED)
-  public SuccessResponse<CocResponseBody> processAndCreateCertificateOfCoverage(
-      @RequestBody @Valid CocRequestBody cocRequestBody) {
+  public SuccessResponse<CertificateResponseDto> createAndSaveCertificateOfCoverage(
+      @RequestBody @Valid CertificateRequestDto certificateRequestDto) {
 
-    CocResponseBody certificateOfCoverage =
-        inventoryService.createAndSaveCertificateOfCoverage(cocRequestBody);
+    CertificateResponseDto certificateOfCoverage =
+        certificateService.createAndSaveCertificate(certificateRequestDto);
 
-    return new SuccessResponse<CocResponseBody>(
+    return new SuccessResponse<CertificateResponseDto>(
         HttpStatus.CREATED.value(),
         HttpStatus.CREATED,
         "Certificate of Coverage is successfully created.",
         certificateOfCoverage);
-  }
-
-  @PostMapping("/addListOfCertificate")
-  @ResponseStatus(HttpStatus.CREATED)
-  public SuccessResponse<List<CocResponseBody>> processAndCreateListCertificateOfCoverage(
-      @RequestBody @Valid List<CocRequestBody> listCocRequestBody) {
-
-    List<CocResponseBody> listCocResponseBody =
-        inventoryService.createAndSaveListCertificateOfCoverage(listCocRequestBody);
-
-    return new SuccessResponse<List<CocResponseBody>>(
-        HttpStatus.CREATED.value(),
-        HttpStatus.CREATED,
-        "Successfully created a batch of Certificate of Coverage.",
-        listCocResponseBody);
-  }
-
-  @GetMapping("/getCertificateOfCoverage/{cocNumber}")
-  @ResponseStatus(HttpStatus.FOUND)
-  public SuccessResponse<CocResponseBody> processAndGetCertificateOfCoverage(
-      @PathVariable("cocNumber") String cocNumber) {
-
-    CocResponseBody coc = inventoryService.retrieveCertificateOfCoverageById(cocNumber);
-
-    return new SuccessResponse<CocResponseBody>(
-        HttpStatus.FOUND.value(),
-        HttpStatus.FOUND,
-        "Successfully retrieved a Certificate of Coverage.",
-        coc);
-  }
-
-  @GetMapping("/paginateCertificates")
-  @ResponseStatus(HttpStatus.FOUND)
-  public CocPageResponse<CocResponseBody> getPaginatedCertificates(
-      @RequestParam(defaultValue = "0") int pageNumber,
-      @RequestParam(defaultValue = "10") int pageSize,
-      @RequestParam(defaultValue = "cocNumber") String sortBy) {
-    return inventoryService.retrieveAndFilterPaginatedCertificates(pageNumber, pageSize, sortBy);
-  }
-
-  @GetMapping("/getAllCertificates")
-  @ResponseStatus(HttpStatus.FOUND)
-  public SuccessResponse<List<CocResponseBody>> getAllCertificates() {
-    List<CocResponseBody> listOfCoc = inventoryService.retrieveAllCertificates();
-
-    return new SuccessResponse<List<CocResponseBody>>(
-        HttpStatus.FOUND.value(),
-        HttpStatus.FOUND,
-        "Successfully retrieved all of the Certificates.",
-        listOfCoc);
   }
 }
