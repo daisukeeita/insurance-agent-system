df8186635e3ad279e2a966ba5239e10b846fe4d1
JAVV <vinas.johnandrei@gmail.com>
2025-12-04 09:14:00 +0800

    modified methods and add comments for documentation.

--- src/main/java/com/acolyptos/insurance/domain/transaction/PolicyRepositoryInterface.java
+++ src/main/java/com/acolyptos/insurance/domain/transaction/PolicyRepositoryInterface.java
@@ -1,17 +1,59 @@
 package com.acolyptos.insurance.domain.transaction;
 
+import com.acolyptos.insurance.domain.certificate.CertificateOfCoverage;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+/** Repository interface for managing {@link Policy} entities. */
 public interface PolicyRepositoryInterface {
 
+  /**
+   * Persists the given {@link Policy} entity to the database.
+   *
+   * @param policy The entity to be saved.
+   * @return The saved {@link Agent} entity.
+   */
   Policy savePolicy(Policy policy);
 
-  List<Policy> saveAllPolicy(List<Policy> listPolicy);
+  /**
+   * Finds a single {@link Policy} entity by its unique identification number.
+   *
+   * @param policyId The unique ID of the policy to find.
+   * @return An {@link Optional} containing the {@link Policy} if found, otherwise empty.
+   */
+  Optional<Policy> findPolicyById(String policyId);
 
-  Optional<Policy> getPolicyById(String policyId);
+  /**
+   * Finds a single {@link Policy} entity by its linked {@link CertificateOfCoverage}.
+   *
+   * @param cocNumber The COC Number or unique identification number of the Certificate of Coverage
+   *     linked on policy to find.
+   * @return An {@link Optional} containing the {@link Policy} if found, otherwise empty.
+   */
+  Optional<Policy> findPolicyByCocNumber(String cocNumber);
 
+  /**
+   * Checks if a {@link Policy} entity with the given unique IID exists in the database.
+   *
+   * @param policyId The unique ID of the Policy to check for existence.
+   * @return 'true' if a policy exists with the given unique ID, otherwise 'false'.
+   */
+  boolean checkPolicyIfExistsById(String policyId);
+
+  /**
+   * Retrieves a paginated list of all {@link Policy} entities.
+   *
+   * @param pageable The pagination information of the Policy (number, size, sort).
+   * @return A {@link Page} of {@link Policy} entities.
+   */
   Page<Policy> getPaginatedPolicy(Pageable pageable);
+
+  /**
+   * Retrieves all {@link Policy} entities from the database.
+   *
+   * @return A list of all {@link Policy} entities.
+   */
+  List<Policy> findAllPolicy();
 }
