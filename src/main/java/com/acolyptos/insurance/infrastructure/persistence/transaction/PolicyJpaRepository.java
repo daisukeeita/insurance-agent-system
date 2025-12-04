ac3825e519669a7b355fa75a7347ed879f1183d4
JAVV <vinas.johnandrei@gmail.com>
2025-12-04 09:33:10 +0800

    added method for checking its existence by linked certificate.

--- src/main/java/com/acolyptos/insurance/infrastructure/persistence/transaction/PolicyJpaRepository.java
+++ src/main/java/com/acolyptos/insurance/infrastructure/persistence/transaction/PolicyJpaRepository.java
@@ -1,6 +1,20 @@
 package com.acolyptos.insurance.infrastructure.persistence.transaction;
 
 import com.acolyptos.insurance.domain.transaction.Policy;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PolicyJpaRepository extends JpaRepository<Policy, String> {}
+/**
+ * Spring Data JPA Repository for the {@link Policy} entity, providding standard CRUD and custom
+ * lookup methods.
+ */
+public interface PolicyJpaRepository extends JpaRepository<Policy, String> {
+
+  /**
+   * Checks if a {@link Policy} entity exists wit the given linked COC Number.
+   *
+   * @param cocNumber The COC Number of the certificate linked to the policy to search for.
+   * @return An {@link Optional} containing the {@link Policy} if found, otherwise empty.
+   */
+  Optional<Policy> findByCocNumber(String cocNumber);
+}
