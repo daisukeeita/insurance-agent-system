bcd793d2510dd3a32e1918f77dba1181ef8dd65d
JAVV <vinas.johnandrei@gmail.com>
2025-12-04 09:33:34 +0800

    modified method implementations and added comments for documentation.

--- src/main/java/com/acolyptos/insurance/infrastructure/persistence/transaction/PolicyJpaRepositoryImplementation.java
+++ src/main/java/com/acolyptos/insurance/infrastructure/persistence/transaction/PolicyJpaRepositoryImplementation.java
@@ -8,32 +8,51 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+/**
+ * The implementation of the {@link PolicyRepositoryInterface} that uses {@link PolicyJpaRepository}
+ * to interact with the database.
+ */
 @Repository
 public class PolicyJpaRepositoryImplementation implements PolicyRepositoryInterface {
 
   private final PolicyJpaRepository policyJpaRepository;
 
-  public PolicyJpaRepositoryImplementation(PolicyJpaRepository policyJpaRepository) {
+  /**
+   * Constructs the repository implementation and injects the necessry JPA component.
+   *
+   * @param policyJpaRepository The Spring Data JPA repository used from direct database access.
+   */
+  public PolicyJpaRepositoryImplementation(final PolicyJpaRepository policyJpaRepository) {
     this.policyJpaRepository = policyJpaRepository;
   }
 
   @Override
-  public Policy savePolicy(Policy policy) {
+  public Policy savePolicy(final Policy policy) {
     return policyJpaRepository.save(policy);
   }
 
   @Override
-  public List<Policy> saveAllPolicy(List<Policy> listPolicy) {
-    return policyJpaRepository.saveAll(listPolicy);
-  }
-
-  @Override
-  public Optional<Policy> getPolicyById(String policyId) {
+  public Optional<Policy> findPolicyById(final String policyId) {
     return policyJpaRepository.findById(policyId);
   }
 
   @Override
-  public Page<Policy> getPaginatedPolicy(Pageable pageable) {
+  public Optional<Policy> findPolicyByCocNumber(final String cocNumber) {
+    return policyJpaRepository.findByCocNumber(cocNumber);
+  }
+
+  @Override
+  public boolean checkPolicyIfExistsById(final String policyId) {
+    return policyJpaRepository.existsById(policyId);
+  }
+
+  @Override
+  public Page<Policy> getPaginatedPolicy(final Pageable pageable) {
     return policyJpaRepository.findAll(pageable);
+  }
+
+  @Override
+  public List<Policy> findAllPolicy() {
+    return policyJpaRepository.findAll();
   }
 }
