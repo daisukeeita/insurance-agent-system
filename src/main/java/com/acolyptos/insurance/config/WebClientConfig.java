7dfbbbe30ebb12684cf705ec71dc2ab89f534a8e
JAVV <vinas.johnandrei@gmail.com>
2025-12-08 10:09:08 +0800

    modified configuration for unsafe connection to the third-party API, with connection and response timeout.

--- src/main/java/com/acolyptos/insurance/config/WebClientConfig.java
+++ src/main/java/com/acolyptos/insurance/config/WebClientConfig.java
@@ -1,9 +1,16 @@
 package com.acolyptos.insurance.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 /** Spring Web Client configuration for calling a third-party API asynchronously. */
 @Configuration
@@ -15,8 +22,8 @@ public class WebClientConfig {
   @Value("${api.thirdparty.connect-timeout}")
   private int connectionTimeountMillis;
 
-  @Value("${api.thirdparty.read-timeount-seconds}")
-  private int readTimeountSeconds;
+  @Value("${api.thirdparty.response-timeount-seconds}")
+  private int responseTimeoutSeconds;
 
   /**
    * Builds a {@code WebClient} connection to the third-part API.
@@ -24,7 +31,25 @@ public class WebClientConfig {
    * @return A {@link WebClient} once the connection is built.
    */
   @Bean
-  public WebClient thirdPartyWebClient() {
-    return WebClient.builder().baseUrl(baseUrl).build();
+  public WebClient insecureWebClient() {
+    SslContext sslContext;
+
+    try {
+      sslContext =
+          SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+    } catch (Exception e) {
+      throw new IllegalStateException("Failed to create insecure SSL context.", e);
+    }
+
+    HttpClient httpClient =
+        HttpClient.create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectionTimeountMillis)
+            .responseTimeout(Duration.ofSeconds(responseTimeoutSeconds))
+            .secure(sslContextSpec -> sslContextSpec.sslContext(sslContext));
+
+    return WebClient.builder()
+        .clientConnector(new ReactorClientHttpConnector(httpClient))
+        .baseUrl(baseUrl)
+        .build();
   }
 }
