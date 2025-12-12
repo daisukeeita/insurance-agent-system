14ec4ab3f8cecf94ac8179e38aec97153c69917b
JAVV <vinas.johnandrei@gmail.com>
2025-12-12 14:02:33 +0800

    added annotation for persistence.

--- src/main/java/com/acolyptos/insurance/application/service/AgentDetailsServiceImplementation.java
+++ src/main/java/com/acolyptos/insurance/application/service/AgentDetailsServiceImplementation.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AgentDetailsServiceImplementation implements UserDetailsService {
@@ -19,6 +20,7 @@ public class AgentDetailsServiceImplementation implements UserDetailsService {
   }
 
   @Override
+  @Transactional(readOnly = true)
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     Agent agent =
         agentRepositoryInterface
